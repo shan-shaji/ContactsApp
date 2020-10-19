3d7d6fdaa49f752966752f2078e735ee5e52dc5b
jisjoe <jis@purplefront.com>
2020-10-19 23:51:06 +0530

    Added android folders

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Material Example Widget',
       theme: ThemeData(
+        backgroundColor: Colors.red,
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
